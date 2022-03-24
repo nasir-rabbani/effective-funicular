6aca7f282e0de583866614080d9482381ba5c877
Harshit Omar <hapyharshit@gmail.com>
2022-03-24 18:30:01 +0530

    have policy as string

--- policy.tf
+++ policy.tf
@@ -1,19 +1,5 @@
 resource "aws_s3_bucket_policy" "harshit_bucket_policy" {
   bucket = aws_s3_bucket.harshit-bucket.id
 
-  policy = <<POLICY
-{
-    "Version": "2012-10-17",
-    "Id": "Policy1609848560531",
-    "Statement": [
-        {
-            "Sid": "Stmt1609848558282",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": "s3:Put*",
-            "Resource": "arn:aws:s3:::harshit-bucket/*"
-        }
-    ]
-}
-POLICY
+  policy = "{\"Statement\":[{\"Action\":\"es:*\",\"Effect\":\"Allow\",\"Principal\":\"*\",\"Resource\":\"arn:aws:es:ca-central-1:641885301384:domain/ptshggaed1/*\"}],\"Version\":\"2012-10-17\"}"
 }
